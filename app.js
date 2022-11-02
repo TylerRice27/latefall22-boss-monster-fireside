826d9876aec4b3add4c6db98ae6e5ee8f82b6bd6
TylerRice27 <Tylerrayrice2@gmail.com>
2022-11-02 12:19:33 -0600

    Adding images and starting touches

---
+++ app.js
@@ -0,0 +1,24 @@
+const heroes = [
+    {
+        name: 'Spider-man',
+        type: 'Rouge',
+        damage: 10,
+        health: 100,
+        img: 'Spider-Man/Spider-Man 00.png'
+    },
+    {
+        name: 'Wolverine',
+        type: 'Tank',
+        damage: 5,
+        health: 100,
+        img: 'Wolverine/Wolverine 01.png'
+    }
+]
+
+
+const boss = {
+    health: 100,
+    maxHealth: 100,
+    damage: 5,
+    level: 1
+}
