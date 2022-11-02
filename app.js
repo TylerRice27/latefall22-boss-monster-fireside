1c163065775de649503d2028d035b570ca6ca42c
TylerRice27 <Tylerrayrice2@gmail.com>
2022-11-02 15:16:05 -0600

    Boss and hero logic

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-const heroes = [
-    {
-        name: 'Spider-man',
-        type: 'Rouge',
-        damage: 10,
-        health: 100,
-        img: 'Spider-Man/Spider-Man 00.png'
-    },
-    // {
-    //     name: 'Wolverine',
-    //     type: 'Tank',
-    //     damage: 5,
-    //     health: 100,
-    //     img: 'Wolverine/Wolverine 01.png'
-    // }
-]
+const hero =
+{
+    name: 'Spider-man',
+    type: 'Rouge',
+    damage: 10,
+    health: 100,
+    credit: 0,
+    img: 'Spider-Man/Spider-Man 00.png'
+}
 
+const companion =
+{
+    name: 'Wolverine',
+    type: 'Tank',
+    damage: 5,
+    health: 100,
+    img: 'Wolverine/Wolverine 01.png'
+}
+
+function update() {
+}
 
 const boss = {
     health: 100,
@@ -24,7 +28,34 @@ const boss = {
 }
 
 function attackBoss() {
-    heroes.forEach(h => h. {
+    if (hero.health <= 0) {
+        return window.alert("You dead bro")
+    }
+    // NOTE This doesn't work because the = sign
+    // is assign the left hand thing to the right hand thing
+    // hero.damage -= boss.health
+    boss.health -= hero.damage
+    if (boss.health < 0) {
+        bossLevelUp()
+        update()
+    }
 
-    });
 }
+
+function bossLevelUp() {
+    boss.level++
+    boss.hp = boss.level * 100
+    hero.credit += boss.level * 100
+
+}
+
+function bossAttack() {
+    boss.damage = boss.damage * boss.level
+    hero.health -= boss.level
+    if (hero.health <= 0) {
+        hero.health = 0
+
+    }
+
+}
+
