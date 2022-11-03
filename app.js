e98486c6bd83d06e3ed2697db5ebb178cc81631d
Jeffrey Watson <watsonjeffrey44@gmail.com>
2022-11-03 14:48:30 -0600

    @ts-ignore

--- app.js
+++ app.js
@@ -1,58 +1,62 @@
 const hero =
 {
-    name: 'Spider-man',
-    type: 'Rouge',
-    damage: 10,
-    health: 100,
-    credit: 0,
+  name: 'Spider-man',
+  type: 'Rouge',
+  damage: 10,
+  health: 100,
+  credit: 0,
 }
 
 const companions = [
 
-    {
-        name: 'Wolverine',
-        type: 'dmg',
-        value: 5,
-        health: 150,
-        cost: 100,
-        power: 0,
-        img: 'https://media2.giphy.com/media/HTy0GcFDvOVi0/giphy.gif'
-    },
+  {
+    name: 'Wolverine',
+    type: 'dmg',
+    value: 5,
+    health: 150,
+    cost: 100,
+    power: 0,
+    img: 'https://media2.giphy.com/media/HTy0GcFDvOVi0/giphy.gif'
+  },
 
-    {
-        name: 'Iron-man',
-        type: 'dmg',
-        value: 15,
-        health: 100,
-        cost: 200,
-        power: 0,
-        img: "https://media1.giphy.com/media/7EiixpldfkXni/giphy.gif?cid=ecf05e470ymdtyll84dy0v9d66xiq0et3sqlzux9sf9ifv9c&rid=giphy.gif&ct=s"
+  {
+    name: 'Iron-man',
+    type: 'dmg',
+    value: 15,
+    health: 100,
+    cost: 200,
+    power: 0,
+    img: "https://media1.giphy.com/media/7EiixpldfkXni/giphy.gif?cid=ecf05e470ymdtyll84dy0v9d66xiq0et3sqlzux9sf9ifv9c&rid=giphy.gif&ct=s"
 
-    },
-    {
-        name: 'Deadpool',
-        type: 'heal',
-        value: 10,
-        health: 100,
-        cost: 500,
-        power: 0,
-        img: "https://media3.giphy.com/media/TAbghspb8GP4I/200w.webp?cid=ecf05e47iusv43aubq8g15qe54v33cvtt327gqjf7nbaaouo&rid=200w.webp&ct=s"
+  },
+  {
+    name: 'Deadpool',
+    type: 'heal',
+    value: 10,
+    health: 100,
+    cost: 500,
+    power: 0,
+    img: "https://media3.giphy.com/media/TAbghspb8GP4I/200w.webp?cid=ecf05e47iusv43aubq8g15qe54v33cvtt327gqjf7nbaaouo&rid=200w.webp&ct=s"
 
-    }
+  }
 ]
 
 const boss = {
-    health: 100,
-    maxHealth: 100,
-    damage: 5,
-    level: 1
+  health: 100,
+  maxHealth: 100,
+  damage: 5,
+  level: 1
 }
 
 function update() {
-    document.getElementById('boss-hp').innerText = boss.health
-    document.getElementById('hero-hp').innerText = hero.health
-    document.getElementById('boss-lvl').innerText = boss.level
-    document.getElementById('credits').innerText = hero.credit
+  // @ts-ignore
+  document.getElementById('boss-hp').innerText = boss.health
+  // @ts-ignore
+  document.getElementById('hero-hp').innerText = hero.health
+  // @ts-ignore
+  document.getElementById('boss-lvl').innerText = boss.level
+  // @ts-ignore
+  document.getElementById('credits').innerText = hero.credit
 
 
 
@@ -60,41 +64,41 @@ function update() {
 
 
 function attackBoss() {
-    if (hero.health <= 0) {
-        window.alert("You dead bro")
-        return
-    }
-    // NOTE This doesn't work because the = sign
-    // is assign the left hand thing to the right hand thing
-    // hero.damage -= boss.health
-    boss.health -= hero.damage
-    hero.credit += boss.level
-    if (boss.health < 0) {
-        bossLevelUp()
-    }
+  if (hero.health <= 0) {
+    window.alert("You dead bro")
+    return
+  }
+  // NOTE This doesn't work because the = sign
+  // is assign the left hand thing to the right hand thing
+  // hero.damage -= boss.health
+  boss.health -= hero.damage
+  hero.credit += boss.level
+  if (boss.health < 0) {
+    bossLevelUp()
+  }
 
-    // let audio = new Audio('imgs/Spider-Man web shoot sound effect.mp3')
-    // audio.play()
-    update()
+  // let audio = new Audio('imgs/Spider-Man web shoot sound effect.mp3')
+  // audio.play()
+  update()
 
 }
 
 function bossLevelUp() {
-    boss.level++
-    boss.maxHealth = boss.level * 100
-    boss.health = boss.maxHealth
-    hero.credit += boss.level * 100
+  boss.level++
+  boss.maxHealth = boss.level * 100
+  boss.health = boss.maxHealth
+  hero.credit += boss.level * 100
 
 }
 
 function bossAttack() {
-    boss.damage = boss.damage * boss.level
-    hero.health -= boss.level
-    if (hero.health <= 0) {
-        hero.health = 0
+  boss.damage = boss.damage * boss.level
+  hero.health -= boss.level
+  if (hero.health <= 0) {
+    hero.health = 0
 
-    }
-    update()
+  }
+  update()
 }
 
 
@@ -107,69 +111,71 @@ setInterval(bossAttack, 2000)
 
 function drawCompanions(companion) {
 
-    let template = ` <div class="d-flex flex-column align-items-center">
+  let template = ` <div class="d-flex flex-column align-items-center">
                 <h3 class="text-light text-center text-shadow">Level: ${companion.power} | HP: ${companion.health}</span></h3>
                 <img class="comp-img " src="${companion.img}" alt="">
 
             </div>`
 
-    document.getElementById(`${companion.name}`).innerHTML = template
+  // @ts-ignore
+  document.getElementById(`${companion.name}`).innerHTML = template
 
 }
 function buy(name) {
-    let companion = companions.find(f => f.name == name)
-    console.log(companion);
+  let companion = companions.find(f => f.name == name)
+  console.log(companion);
 
-    if (hero.credit < companion.cost) {
-        window.alert("Go farm some more credits")
-        return
-    }
-    // @ts-ignore
-    hero.credit -= companion.cost
-    // @ts-ignore
-    companion.power++
+  // @ts-ignore
+  if (hero.credit < companion.cost) {
+    window.alert("Go farm some more credits")
+    return
+  }
+  // @ts-ignore
+  hero.credit -= companion.cost
+  // @ts-ignore
+  companion.power++
 
-    drawCompanions(companion)
-    companionsActions()
-    update()
+  drawCompanions(companion)
+  companionsActions()
+  update()
 }
 
 function damageBoss(damage) {
-    boss.health -= damage
-    if (boss.health < 0) {
-        bossLevelUp()
-    }
-    update()
+  boss.health -= damage
+  if (boss.health < 0) {
+    bossLevelUp()
+  }
+  update()
 }
 
 function healHero(val) {
-    hero.health += val
-    if (hero.health >= 100) {
-        hero.health = 100
-    }
-    update()
+  hero.health += val
+  if (hero.health >= 100) {
+    hero.health = 100
+  }
+  update()
 }
 
 function companionsActions() {
-    companions.forEach(c => {
+  companions.forEach(c => {
 
-        console.log(c);
-        if (c.health <= 0) {
-            c.health = 0
-            return
-        }
-        if (c.type == "dmg") {
-            let damage = c.power * c.value
-            damageBoss(damage)
-        } else {
-            let heal = c.power * c.value
-            healHero(heal)
-        }
+    console.log(c);
+    if (c.health <= 0) {
+      c.health = 0
+      return
     }
+    if (c.type == "dmg") {
+      let damage = c.power * c.value
+      damageBoss(damage)
+    } else {
+      let heal = c.power * c.value
+      healHero(heal)
+    }
+  }
 
-    )
+  )
 
-    update()
+  update()
 }
 
 
